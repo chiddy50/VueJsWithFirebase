e946eca50e3e109ccf5f32cfd9ea3d05f9262b43
chiddy50 <megasanchez50@gmail.com>
2020-02-28 13:10:58 +0100

    Final commit

--- src/components/firebaseInit.js
+++ src/components/firebaseInit.js
@@ -1,8 +1,8 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
+import firebase from 'firebase';
+import 'firebase/firestore';
 
-import firebaseConfig from './firebaseConfig'
+import firebaseConfig from './firebaseConfig';
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default firebaseApp.firestore()
+export default firebaseApp.firestore();
